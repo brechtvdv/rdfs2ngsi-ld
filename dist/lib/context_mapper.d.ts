62253550341086498f20329b40572befcf605a02
Julian Rojas <julian.rojas87@gmail.com>
2023-04-19 01:28:26 +0200

    Include context_mapper in built sources

---
+++ dist/lib/context_mapper.d.ts
@@ -0,0 +1,1 @@
+export declare const mapType: (type: string) => string;
---
+++ dist/lib/context_mapper.js
@@ -0,0 +1,42 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.mapType = void 0;
+// hardcoded map of type values to use during transformation
+// from the OSLO Passenger Hubs semantic data model to the OSLO SDM
+const map = {
+    ///// type: ResourceReport
+    "https://purl.eu/ns/mobility/passenger-transport-hubs#ResourceReport": "https://smartdatamodels.org/dataModel.OSLO/ResourceReport",
+    // ResourceReport.actuator
+    "https://purl.eu/ns/mobility/passenger-transport-hubs#propulsion": "https://smartdatamodels.org/dataModel.OSLO/ResourceReport.actuator",
+    // ResourceReport.location
+    "https://purl.eu/ns/mobility/passenger-transport-hubs#location": "https://smartdatamodels.org/dataModel.OSLO/ResourceReport.location",
+    // ResourceReport.meansOfTransport
+    "https://purl.eu/ns/mobility/passenger-transport-hubs#Mobiliteitsdienst.vervoermiddel": "https://smartdatamodels.org/dataModel.OSLO/ResourceReport.meansOfTransport",
+    // ResourceReport.number
+    "https://purl.eu/ns/mobility/passenger-transport-hubs#number": "https://smartdatamodels.org/dataModel.OSLO/ResourceReport.number",
+    // ResourceReport.service
+    "https://purl.eu/ns/mobility/passenger-transport-hubs#service": "https://smartdatamodels.org/dataModel.OSLO/ResourceReport.service",
+    // ResourceReport.status
+    "https://purl.eu/ns/mobility/passenger-transport-hubs#status": "https://smartdatamodels.org/dataModel.OSLO/ResourceReport.status",
+    // ResourceReport.type
+    "http://purl.org/dc/terms/type": "https://smartdatamodels.org/dataModel.OSLO/ResourceReport.type",
+    // ResourceReport.reportTime
+    "http://purl.org/dc/elements/1.1/date": "https://smartdatamodels.org/dataModel.OSLO/ResourceReport.reportTime",
+    ///// type: BicycleParkingStation
+    "http://schema.mobivoc.org/#BicycleParkingStation": "https://smartdatamodels.org/dataModel.OSLO/BicycleParkingStation",
+    // name
+    "http://schema.org/name": "https://smartdatamodels.org/name",
+    // ParkingFacility.capacity
+    "http://schema.org/capacity": "https://smartdatamodels.org/dataModel.OSLO/ParkingFacility.capacity",
+    // InfrastructureElement.geometry
+    "http://www.w3.org/ns/locn#geometry": "https://smartdatamodels.org/dataModel.OSLO/InfrastructureElement.geometry",
+    ///// type: Capacity (This type definition is missing in the SDM)
+    "http://schema.org/Capacity": "https://smartdatamodels.org/dataModel.OSLO/Capacity",
+    // Capacity.total (This property definition is missing in the SDM)
+    "http://schema.org/totalCapacity": "https://smartdatamodels.org/dataModel.OSLO/Capacity.total"
+};
+const mapType = (type) => {
+    return map[type] || type;
+};
+exports.mapType = mapType;
+//# sourceMappingURL=context_mapper.js.map
